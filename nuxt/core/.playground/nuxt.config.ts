c49ee6413d0aa62b6cc93f39c81218b7d7dabc13
deetz99 <dietrich@wolpert.ca>
2024-07-17 11:13:51 -0700

    fix dev config interfering with parent app.

--- nuxt/core/.playground/nuxt.config.ts
+++ nuxt/core/.playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   extends: ['..'],
-  modules: ["@nuxt/image"],
+  modules: ['@nuxtjs/eslint-module', '@nuxt/test-utils/module', "@nuxt/image"],
   compatibilityDate: "2024-07-16",
   future: {
     compatibilityVersion: 4
--- nuxt/core/nuxt.config.ts
+++ nuxt/core/nuxt.config.ts
@@ -1,53 +1,52 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
   devtools: { enabled: false },
 
-  compatibilityDate: "2024-07-16",
+  compatibilityDate: '2024-07-16',
 
   future: {
     compatibilityVersion: 4
   },
 
-  css: [join(currentDir, "./app/assets/css/main.css")],
+  css: [join(currentDir, './app/assets/css/main.css')],
 
   modules: [
-    "@nuxt/ui",
-    "@nuxt/content",
-    "@pinia/nuxt",
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
-    "@nuxtjs/i18n",
-    "@vueuse/nuxt",
-    '@nuxtjs/eslint-module',
-    '@nuxt/test-utils/module'
+    '@nuxtjs/i18n',
+    '@vueuse/nuxt'
+    // '@nuxt/test-utils/module'
   ],
 
   ui: {
-    icons: ["mdi"],
+    icons: ['mdi']
   },
 
   alias: {
-    BCGovFonts: join(currentDir, "./public/fonts/BCSans"),
+    BCGovFonts: join(currentDir, './public/fonts/BCSans'),
     BCGovLogoSmEn: join(
       currentDir,
-      "./public/BCGovLogo/gov_bc_logo_vert_en.png"
+      './public/BCGovLogo/gov_bc_logo_vert_en.png'
     ),
     BCGovLogoSmFr: join(
       currentDir,
-      "./public/BCGovLogo/gov_bc_logo_vert_fr.png"
+      './public/BCGovLogo/gov_bc_logo_vert_fr.png'
     ),
     BCGovLogoLgEn: join(
       currentDir,
-      "./public/BCGovLogo/gov_bc_logo_horiz_en.png"
+      './public/BCGovLogo/gov_bc_logo_horiz_en.png'
     ),
     BCGovLogoLgFr: join(
       currentDir,
-      "./public/BCGovLogo/gov_bc_logo_horiz_fr.png"
-    ),
+      './public/BCGovLogo/gov_bc_logo_horiz_fr.png'
+    )
   },
 
   colorMode: {
