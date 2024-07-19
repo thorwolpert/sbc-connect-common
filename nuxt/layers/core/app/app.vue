63288a7861e291dff8e0f5abc7caebb6a90379aa
deetz99 <dietrich@wolpert.ca>
2024-07-19 12:49:04 -0700

    add default layout and dev page

--- nuxt/layers/core/app/app.vue
+++ nuxt/layers/core/app/app.vue
@@ -1,15 +1,5 @@
-<script setup lang="ts">
-const { isAuthenticated, login, logout } = useKeycloak()
-</script>
 <template>
-  <div class="mx-auto flex flex-col bg-gray-500">
-    <ConnectHeader />
-    <div> hello </div>
-    <ClientOnly>
-      <UButton v-if="!isAuthenticated" label="Login" @click="login(IdpHint.BCSC)" />
-      <UButton v-else-if="isAuthenticated" label="Logout" @click="logout()" />
-      <div> {{ isAuthenticated }} </div>
-    </ClientOnly>
-    <ConnectFooter />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ nuxt/layers/core/app/pages/layer-dev-playground.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { isAuthenticated, login, logout } = useKeycloak()
+</script>
+<template>
+  <div class="flex flex-col gap-8 border border-black px-2 py-8">
+    <h1 class="text-3xl font-semibold text-bcGovColor-darkGray">
+      Testing
+    </h1>
+    <ClientOnly>
+      <UButton v-if="!isAuthenticated" label="Login" @click="login(IdpHint.BCSC)" />
+      <UButton v-else-if="isAuthenticated" label="Logout" @click="logout()" />
+      <div> {{ isAuthenticated }} </div>
+    </ClientOnly>
+  </div>
+</template>
