fb2350272fb5737e57c4674ed9a8b8b8807ee6a5
deetz99 <dietrich@wolpert.ca>
2024-07-23 15:42:21 -0700

    check notifications in middleware

--- nuxt/layers/core/app/middleware/load-accounts.global.ts
+++ nuxt/layers/core/app/middleware/load-accounts.global.ts
@@ -1,10 +1,13 @@
 export default defineNuxtRouteMiddleware(async () => {
-  if (import.meta.client) {
-    const { isAuthenticated } = useKeycloak()
+  if (import.meta.client) { // only run on client
+    const { isAuthenticated, kcUser } = useKeycloak()
     if (isAuthenticated.value) {
       const accountStore = useConnectAccountStore()
-      if (accountStore.userAccounts.length === 0) {
+      if (accountStore.userAccounts.length === 0) { // only try loading accounts if there is none
         await accountStore.setAccountInfo()
+      }
+      if (accountStore.currentAccount.id && kcUser.value.keycloakGuid) { // check for pending approvals
+        await accountStore.getPendingApprovalCount(parseInt(accountStore.currentAccount.id), kcUser.value.keycloakGuid)
       }
     }
   }
