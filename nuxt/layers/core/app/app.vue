8ada94792336661d12b47eb8d0c0914bb33b29e0
deetz99 <dietrich@wolpert.ca>
2024-07-26 16:07:32 -0700

    set lang and dir attrs on head

--- nuxt/layers/core/app/app.vue
+++ nuxt/layers/core/app/app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+const i18nHead = useLocaleHead({
+  addDirAttribute: true,
+  addSeoAttributes: true
+})
+
+// set lang and dir attributes on head
+useHead({
+  htmlAttrs: {
+    lang: () => i18nHead.value.htmlAttrs!.lang,
+    dir: () => i18nHead.value.htmlAttrs!.dir
+  }
+})
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
