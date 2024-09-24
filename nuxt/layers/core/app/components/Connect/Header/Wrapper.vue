f10c81650778bfd2bb79faeb1b5c65bb2d0255a4
deetz99 <dietrich@wolpert.ca>
2024-09-24 16:55:51 -0700

    separate header components

---
+++ nuxt/layers/core/app/components/Connect/Header/Wrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <header
+    id="connect-header-wrapper"
+    data-testid="connect-header-wrapper"
+    class="relative border-b-2 border-bcGovColor-navDivider bg-bcGovColor-header p-2 sm:px-4 dark:border-b dark:bg-bcGovColor-darkGray"
+  >
+    <nav
+      class="m-auto w-full max-w-bcGovLg"
+      :aria-label="$t('ConnectHeader.navLabel')"
+    >
+      <slot />
+    </nav>
+  </header>
+</template>
